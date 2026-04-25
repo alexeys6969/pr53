1c9c72cc0a4d4e6a1d5daa9ad5e1fa1e60b6cb0b
Алексей Шашин <alexshashin.2023@gmail.com>
2026-04-25 13:39:16 +0500

    Разработанный класс подключения к БД. Шашин

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Shashin.Classes.Common
+{
+    public class Connection
+    {
+        public static string congig = "server=127.0.0.1;port=3307uid=root;pwd=;database=journal";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(congig);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
